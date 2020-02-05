810b5523070904aff8c316da89c63c09e8f811bf
Huawei Xue <453141835@qq.com>
2020-02-06 01:15:27 +0800

    demo

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector('img')
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src')
+    if(mySrc==='images/无标题.png'){
+        myImage.setAttribute('src','images/download.jpg');
+
+    }else{
+        myImage.setAttribute('src','images/无标题.png');
+
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = '酷毙了，' + myName;
+    }
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '酷毙了，' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
